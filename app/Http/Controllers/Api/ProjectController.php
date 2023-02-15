368d72c6f6aa5d19e3e66d2cd545773419417e9c
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2023-02-15 14:34:33 +0100

    add

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class ProjectController extends Controller
+{
+    public function index(Request $request)
+    {
+
+        $projects = Project::with( "types", "technologies")->paginate(20);
+
+        return response()->json($projects);
+    }
+
+    /* public function show($id) {
+        $post = Post::with("user", "category", "tags")->findOrFail($id);
+        return response()->json($post);
+    } */
+
+    public function show(Project $project)
+    {
+        // come il with() caricare i dati di queste relazioni,
+        // dopo aver eseguito la query principale
+        $project->load("types", "technologies");
+
+        return response()->json($project);
+    }
+}
